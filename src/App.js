c2223f88768090fd792345a02c0aaf96a144ab7e
Zafer <khourdaji@gmail.com>
2019-05-06 18:04:29 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      name: 'zafer',
+      greeting: 'Hello world!'
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <h2>{this.state.greeting}</h2>
+        <p>{this.state.name}</p>
+        {
+          this.state.list.map(item => {
+            // onMyClick is not attached to 'this'
+            const onMyClick = () => this.onDismiss(item.objectID);
+            
+            return (
+              <div key={item.objectID}>
+                <span><a href={item.url}>{item.title}</a></span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <button
+                    onClick={onMyClick}
+                    type="button"
+                  >
+                    Dismiss
+                </button>
+                </span>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
