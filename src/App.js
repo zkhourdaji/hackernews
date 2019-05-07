235107c13c6bf472149354269d81d65049c33c8f
Zafer <khourdaji@gmail.com>
2019-05-07 10:09:05 -0400

    Split App component, add events to filter the list of items

--- src/App.js
+++ src/App.js
@@ -20,35 +20,48 @@ const list = [
   }
 ]
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      list,
-      name: 'zafer',
-      greeting: 'Hello world!'
-    };
-    this.onDismiss = this.onDismiss.bind(this);
-  }
-
-  onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
-  }
+/*
+This function is used to return a function to pass it to the filter method when the 
+user types in the text box. The value of the text box is passed to isSearchTerm as searchTerm
+item will be filled in by the filter function and it represents each item from the list above.
+*/
+const isSearched = (searchTerm) => (item) =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 
-  render() {
+
+class Search extends Component {
+
+  render (){
+
+    const {value, onChange} = this.props; 
 
     return (
-      <div className="App">
-        <h2>{this.state.greeting}</h2>
-        <p>{this.state.name}</p>
-        {
-          this.state.list.map(item => {
+      <form>
+      <input 
+        type='text'
+        value={value}
+        onChange={onChange}
+      />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+
+  render(){
+    const {pattern, list, onDismiss} = this.props;
+
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => {
             // onMyClick is not attached to 'this'
-            const onMyClick = () => this.onDismiss(item.objectID);
-            
+            // we cannot just set the click listener to this.onDismiss(item.objectId) because this will
+            // execute the function right away and only the first time the componenet is rendered.
+            // instead we wrap it in a lambda expressions, this wont be invoked right away.
+            const onMyClick = () => onDismiss(item.objectID);
+            // this return is for the map method
             return (
               <div key={item.objectID}>
                 <span><a href={item.url}>{item.title}</a></span>
@@ -65,7 +78,60 @@ class App extends Component {
                 </span>
               </div>
             )
-          })
+          })}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      name: 'zafer',
+      greeting: 'Hello world!',
+      searchTerm: ''
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  /*
+  This function is called when the user clicks a dismiss button on one of the items.
+  */
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event){
+    this.setState({searchTerm:event.target.value});
+  }
+
+
+  render() {
+
+    // this is ES6 destructing syntax.
+    const {searchTerm, greeting, name, list} = this.state;
+
+    return (
+      <div className="App">
+        <h2>{greeting}</h2>
+        <p>{name}</p>
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+ 
+        {
+          
         }
       </div>
     )
