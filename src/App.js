a4d7a9c92092c592f0b1cc99a377731a194f25fe
Zafer <khourdaji@gmail.com>
2019-05-07 11:07:38 -0400

    Refactor stateless components into functions

--- src/App.js
+++ src/App.js
@@ -29,80 +29,75 @@ const isSearched = (searchTerm) => (item) =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 
-class Button extends Component {
+function Button(props) {
+  const {
+    onClick,
+    className = "", //this is a default value applied if the parent componenet didnt pass a className
+    children
+  } = props;
 
-  render(){
-    const{
-      onClick, 
-      className = "", //this is a default value applied if the parent componenet didnt pass a className
-      children
-    } = this.props;
-    
-    return(
-      <button
-        onClick={onClick}
-        className={className}
-        type='button'
-      >
-        {children}
-      </button>
-    );
-  }
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      type='button'
+    >
+      {children}
+    </button>
+  );
 }
 
 
-class Search extends Component {
 
-  render (){
+function Search(props) {
 
-    const {value, onChange, children} = this.props; 
+  const { value, onChange, children } = props;
 
-    return (
-      <form>
-        {children}
-      <input 
+  return (
+    <form>
+      {children}
+      <input
         type='text'
         value={value}
         onChange={onChange}
       />
-      </form>
-    );
-  }
+    </form>
+  );
 }
 
-class Table extends Component {
 
-  render(){
-    const {pattern, list, onDismiss} = this.props;
 
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => {
-            // onMyClick is not attached to 'this'
-            // we cannot just set the click listener to this.onDismiss(item.objectId) because this will
-            // execute the function right away and only the first time the componenet is rendered.
-            // instead we wrap it in a lambda expressions, this wont be invoked right away.
-            const onMyClick = () => onDismiss(item.objectID);
-            // this return is for the map method
-            return (
-              <div key={item.objectID}>
-                <span><a href={item.url}>{item.title}</a></span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-                  <Button
-                    onClick={onMyClick}
-                  >
-                    Dismiss
-                  </Button>
-                </span>
-              </div>
-            )
-          })}
-      </div>
-    );
-  }
+function Table(props) {
+
+  const { pattern, list, onDismiss } = props;
+
+  return (
+    <div>
+      {list.filter(isSearched(pattern)).map(item => {
+        // onMyClick is not attached to 'this'
+        // we cannot just set the click listener to this.onDismiss(item.objectId) because this will
+        // execute the function right away and only the first time the componenet is rendered.
+        // instead we wrap it in a lambda expressions, this wont be invoked right away.
+        const onMyClick = () => onDismiss(item.objectID);
+        // this return is for the map method
+        return (
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button
+                onClick={onMyClick}
+              >
+                Dismiss
+              </Button>
+            </span>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 class App extends Component {
@@ -127,29 +122,29 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
-  onSearchChange(event){
-    this.setState({searchTerm:event.target.value});
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
 
   render() {
 
     // this is ES6 destructing syntax.
-    const {searchTerm, greeting, name, list} = this.state;
+    const { searchTerm, greeting, name, list } = this.state;
 
     return (
       <div className="App">
         <h2>{greeting}</h2>
         <p>{name}</p>
-        
-        <Search 
+
+        <Search
           value={searchTerm}
           onChange={this.onSearchChange}
         >
-        {/*This will be passed in as props.children available in the Search Componenet*/}
-        Search Component
+          {/*This will be passed in as props.children available in the Search Componenet*/}
+          Search Component
         </Search>
-        <Table 
+        <Table
           list={list}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
