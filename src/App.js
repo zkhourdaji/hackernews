6d87487a46241aa48fb4a399599b2d39654a80ea
Zafer <khourdaji@gmail.com>
2019-05-07 16:08:40 -0400

    Add client side caching capabilities

--- src/App.js
+++ src/App.js
@@ -11,27 +11,6 @@ const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-]
-
 /*
 This function is used to return a function to pass it to the filter method when the 
 user types in the text box. The value of the text box is passed to isSearchTerm as searchTerm
@@ -52,11 +31,11 @@ const Button = ({ onClick, className = "", children }) =>
     {children}
   </button>
 
-const Search = ({ 
-  value, 
+const Search = ({
+  value,
   onChange,
-  onSubmit, 
-  children 
+  onSubmit,
+  children
 }) =>
   <form onSubmit={onSubmit}>
     {children}
@@ -68,9 +47,9 @@ const Search = ({
     <button>Search</button>
   </form>
 
-const largeColumn = {width: '40%'};
-const midColumn = {width: '30%'};
-const smallColumn = {width:'10%'};
+const largeColumn = { width: '40%' };
+const midColumn = { width: '30%' };
+const smallColumn = { width: '10%' };
 
 const Table = ({ list, onDismiss }) =>
   <div className='table'>
@@ -80,7 +59,7 @@ const Table = ({ list, onDismiss }) =>
       // execute the function right away and only the first time the componenet is rendered.
       // instead we wrap it in a lambda expressions, this wont be invoked right away.
       const onMyClick = () => onDismiss(item.objectID);
-      
+
       // this return is for the map method
       return (
         <div key={item.objectID} className='table-row'>
@@ -116,35 +95,44 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [],
       name: 'zafer',
       greeting: 'Hello world!',
+      // search term changes as use types in the text box
       searchTerm: DEFAULT_QUERY,
-      result:null
+      results: null,
+      // searchKey is for caching results, the key will be what the user searched on
+      searchKey: ''
     };
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+  }
+
+  needsToSearchTopStories(searchTerm){
+    return !this.state.results[searchTerm];
   }
 
   // this method is used after we get a json response from the api call
   // result is the json result from the api call
-  setSearchTopStories(result){
+  setSearchTopStories(result) {
 
-    const {hits, page} = result;
-    // if this is the very first page, then oldHits is an empty array
-    // otherwise oldHits will contain the history of the hits
-    const oldHits = page !== 0 ? this.state.result.hits:[];
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
     // concatenate the old hits with the new hits
     const updatedHits = [...oldHits, ...hits];
 
-    // this is a shortcut for {result:result}
     this.setState({
-      result: {
-        hits: updatedHits, 
-        page
+      results: {
+        ...results,
+        [searchKey]: {
+          hits: updatedHits,
+          page
+        }
       }
     });
   }
@@ -153,43 +141,61 @@ class App extends Component {
   This function is called when the user clicks a dismiss button on one of the items.
   */
   onDismiss(id) {
-    const updatedHits = this.state.result.hits.filter(item => item.objectID !== id);
-    this.setState({ 
-     // result: Object.assign({}, this.state.result, {hits: updatedHits})
-     result: {...this.state.result, hits: updatedHits}
+    const {searchKey, results} = this.state;
+    const {hits, page} = results[searchKey];
+    const isNotId = item => item.objectID !== id;
+
+    const updatedHits = hits.filter(isNotId);
+    this.setState({
+      results:{
+        ...results,
+        [searchKey]:{
+          hits:updatedHits,
+          page
+        }
+      }
     });
   }
 
+  // this is used to keep track of the user input in the text box
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
-  
-  onSearchSubmit(event){
-    const{searchTerm} = this.state;
-    this.fetchSearchTopStories(searchTerm);
+
+  // this method is called when the user clicks the submit button
+  // it will call the api with the search term
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    // we keep track of the search key for caching the results to the state
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)){
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault();
   }
 
-  fetchSearchTopStories(searchTerm, page = 0){
+  fetchSearchTopStories(searchTerm, page = 0) {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-    .then(res => res.json())
-    // setSearchTopStories will update the state causing a rerender
-    .then(result => this.setSearchTopStories(result))
-    .catch(error => error);
+      .then(res => res.json())
+      // setSearchTopStories will update the state causing a rerender
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // destructuring sytax
-    const{searchTerm} = this.state;
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
   render() {
     // this is ES6 destructing syntax.
-    const { searchTerm, greeting, name, result } = this.state;
+    const { searchTerm, greeting, name, results, searchKey } = this.state;
     // page will be the page number if result is not null
     // otherwise default to 0 (this is for the first api call)
-    const page = (result && result.page) || 0;
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
     return (
       <div className="page">
         <div className='interactions'>
@@ -205,17 +211,13 @@ class App extends Component {
             Search Component
         </Search>
         </div>
-        {
-          // wow this is crazy shit right here
-          result && 
-          <Table
-          list={result.hits}
+        <Table
+          list={list}
           onDismiss={this.onDismiss}
         />
-        }
         {/* pagination */}
         <div className='interactions'>
-          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page+1)}>
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
             More
           </Button>
         </div>
