73f8adc5f2125ef5cbb6f954a003a2541917b10b
Zafer <khourdaji@gmail.com>
2019-05-08 09:24:23 -0400

    Add and use PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -14,7 +15,7 @@ const PARAM_HPP = 'hitsPerPage=';
 
 //className has a default value 
 //its applied if the parent componenet didnt pass a className
-const Button = ({ onClick, className = "", children }) =>
+const Button = ({ onClick, className, children }) =>
   <button
     onClick={onClick}
     className={className}
@@ -22,6 +23,16 @@ const Button = ({ onClick, className = "", children }) =>
   >
     {children}
   </button>
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
+Button.defaultProps = {
+  className: ''
+};
 
 const Search = ({
   value,
@@ -38,6 +49,14 @@ const Search = ({
     />
     <button>Search</button>
   </form>
+
+Search.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node
+
+}
 
 const largeColumn = { width: '40%' };
 const midColumn = { width: '30%' };
@@ -81,6 +100,19 @@ const Table = ({ list, onDismiss }) =>
       )
     })}
   </div>
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
 
 class App extends Component {
 
